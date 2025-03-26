3372db5e26c7c9c71f0b19248c2e3d9d9a602064
Thendo <tnetshandama90@gmail.com>
2025-03-26 15:38:07 +0200

    My Python Assignment W2

---
+++ Assignment Python.py
@@ -0,0 +1,19 @@
+#Question 1
+My_list = []
+#Question 2
+My_list.append(10)
+My_list.append(20)
+My_list.append(30)  
+My_list.append(40)
+#Question 3
+My_list.insert(1, 15)
+#Question_4
+UR_list = [50, 60, 70]
+My_list.extend(UR_list)
+#Question_5
+del My_list[7]
+#Question_6
+My_list.sort()
+#Question_7
+index = My_list.index(30) 
+print('index,',index)
